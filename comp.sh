6842be4fd2db569fb90217104ba838983ba90ee7
Horatio Caine <hxcaine@gmail.com>
2012-10-19 16:11:00 +0100

    Slight change to compile script.

--- comp.sh
+++ comp.sh
@@ -5,14 +5,14 @@ BUILDCLASSPATH="./gnuprologjava-0.2.6.jar"
 BUILDMAIN="./src/epics/camwin/Main.java"
 DASH="\n--------------------------------------\n"
 
-if [ -d ./build ]; then
+if [ -d $BUILDDIR ]; then
    echo $DASH"Deleting previous build dir"$DASH
-   rm -r ./build
+   rm -r .$BUILDDIR
 fi
 
-mkdir build
-
+mkdir $BUILDDIR
 javac -sourcepath src -classpath $BUILDCLASSPATH -d $BUILDDIR $BUILDMAIN
+
 if [ "$?" -eq 0 ]; then
    echo $DASH"Compile successful"$DASH
    unzip gnuprologjava-0.2.6.jar -d build
@@ -20,13 +20,18 @@ if [ "$?" -eq 0 ]; then
    jar -cvmf ../Manifest.txt ../$JARNAME .
    JARSUCCESS=$?
    cd ..
+
+   if [ "$JARSUCCESS" -eq 0 ]; then
+       echo $DASH"Jar built. Setting perms..."$DASH
+       chmod u+x ./$JARNAME
+   fi
 else
    echo $DASH"Compile failed"$DASH
 fi
 
-if [ "$JARSUCCESS" -eq 0 ]; then
-    echo $DASH"Jar built. Setting perms..."$DASH
-    chmod u+x ./$JARNAME
+if [ -d $BUILDDIR ]; then
+    rm -r $BUILDDIR
+    echo "Build directory deleted"
 fi
 
 echo "Done."
