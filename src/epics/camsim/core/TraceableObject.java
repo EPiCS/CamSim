14676f404465fc874ba94562a199c0b799e6959c
Horatio Caine <hxcaine@gmail.com>
2013-02-24 13:23:19 +0000

    Cleaned up object movements (bouncing off walls) in TraceableObject
    - There was some truly odd behaviour when objects bounced off walls. For
    example, if x exceeded the boundary, it would not increase past the wall
    and, after one time step, it would start moving in the opposite
    direction. However, the y value would continue to move in its original
    direction for one time step. This is all fixed now
    - Cleaned up turnaround angle code from four if-statements into one
    clean one, as well as adjusting x and y by the correct value
    - Changed the bounce angle code to be more moderate and make more sense.
    Now, the angle will flip by 180 degrees, then change by a factor of + or
    - 30 degrees. The previous code would only do positive and not negative
    angle changes.

--- src/epics/camsim/core/TraceableObject.java
+++ src/epics/camsim/core/TraceableObject.java
@@ -62,9 +62,8 @@ public class TraceableObject{
         if (hasWaypoints()){
 
             double dist = Point2D.distance(this.x, this.y, waypoints.get(currentWaypoint).getX(), waypoints.get(currentWaypoint).getY());
-            //System.out.println("     DISTANCE     " + dist);
-            if (dist < 1){
-                //System.out.println(" CHANGING WAYPOINT !!");
+            double epsilon = 0.00001;
+            if (dist < (1 - epsilon)){ // Less than one (with epsilon) 
                 this.currentWaypoint = this.currentWaypoint + 1;
                 if ( this.currentWaypoint >= this.waypoints.size()){
                     this.currentWaypoint = 0;
@@ -73,61 +72,40 @@ public class TraceableObject{
 
             //System.out.println( "WAYPOINT n=" + this.currentWaypoint + " of " + this.waypoints.size() + " [x" + waypoints.get(currentWaypoint).getX() + ", y" +waypoints.get(currentWaypoint).getY() + " ]");
 
-            //System.out.println( "POS: x:" + this.x + " y:" + this.y );
-
-            /*
-             * Direction we should be going
-             */
-            double dir_x = waypoints.get(currentWaypoint).getX() -this.x;
-            double dir_y = waypoints.get(currentWaypoint).getY() -this.y;
-
-            //System.out.println( "DIR: dir_x:" + dir_x + " dir_y:" + dir_y );
-
-            // Dot product of [0,1] vector and our direction
-            double dot = 0 * dir_x + 1 * dir_y;
+            double delta_x = waypoints.get(currentWaypoint).getX() - this.x;
+            double delta_y = waypoints.get(currentWaypoint).getY() - this.y;
 
             // Magic way to compute our new heading :)
-            double ang = ( Math.atan2( 1, 0 ) - Math.atan2( dir_y, dir_x ) ) * 180 / Math.PI;
+            // Radians
+            this.heading = Math.atan2(1,0) - Math.atan2(delta_y, delta_x);
 
-            //System.out.println( "ANG: " + ang );
-
-            this.heading = ang;
-
-            x_move = Math.sin( (heading * Math.PI) / 180 ) * speed;
-            y_move = Math.cos( (heading * Math.PI) / 180 ) * speed;
+            x_move = Math.sin(this.heading) * speed;
+            y_move = Math.cos(this.heading) * speed;
         } else {
-            x_move = Math.sin( heading ) * speed;
-            y_move = Math.cos( heading ) * speed;
+            x_move = Math.sin(this.heading) * speed;
+            y_move = Math.cos(this.heading) * speed;
         }
-
-        //System.out.println( "MOVE: x:" + x_move + " y:" + y_move );
 
         this.x += x_move;
         this.y += y_move;
 
-        if ( this.x > sim.get_max_x() ){
-            //System.out.println( "       KICKING IN       ");
-            this.heading += getTurnaroundAngle(); this.x -= speed;
-        }
-        if ( this.x < sim.get_min_x() ){
-            //System.out.println( "       KICKING IN       ");
-            this.heading += getTurnaroundAngle(); this.x += speed;
-        }
-
-        if ( this.y > sim.get_max_y() ){
-            //System.out.println( "       KICKING IN       ");
-            this.heading += getTurnaroundAngle(); this.y -= speed;
-        }
-        if ( this.y < sim.get_min_y() ){
-            //System.out.println( "       KICKING IN       ");
-            this.heading += getTurnaroundAngle(); this.y += speed;
+        // If we breach any boundary, bounce off at a slightly randomised angle
+        if (this.x > sim.get_max_x() || this.x < sim.get_min_x() ||
+        		this.y > sim.get_max_y() || this.y < sim.get_min_y()){
+            this.heading += getTurnaroundAngle(); 
+            
+            // Undo move across boundary
+            this.x -= x_move; 
+            this.y -= y_move;
         }
     }
 
     /** Gets an angle to turn around by with some partial added randomness */
     public double getTurnaroundAngle() {
     	// Turn around 180 degrees, add a bit of angle for randomness
-    	return Math.PI + RandomNumberGenerator.nextDouble(RandomUse.USE.TURN) * Math.PI / 6.0;
+    	double angle = Math.PI; // Turn 180 degrees
+    	angle += (RandomNumberGenerator.nextDouble(RandomUse.USE.TURN)*2-1.0) * Math.PI / 6.0;
+    	return angle;
     }
     
     public double getX(){
